3a04eb83e394bd943c9731dbbd6959670708ef1e
manav girnara <manavgirnara@gmail.com>
2026-02-02 12:05:34 +0530

    changes are added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,51 +1,152 @@
-import { Link } from "react-router-dom";
 import React from "react";
+import "../index.css";
 import "./Register.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Register = () => {
-    return (
-    <>
-        <div className="form-container">
-        {/* Page title */}
-        <h1 className="form-title">Register Page</h1>
-        <form>
-          {/* Name field */}
-            <div className="form-group">
-            <label htmlFor="name">Full Name</label>
-            <input type="text" id="name" name="Full name" placeholder="Enter your full name" />
-            </div>
-          {/* Email field */}
-            <div className="form-group">
-            <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" />
-            </div>
-          {/* Phone number field */}
-            <div className="form-group">
-            <label htmlFor="phone">Phone Number</label>
-            <input
-                type="tel"
-                id="phone"
-                name="phone"
-                placeholder="Enter your phone number"
-            />
-            </div>
-            {/* Password field */}
-            <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="Password" placeholder="Enter your password" />
-            </div>
-            {/*submit Button*/}
-            <button type="submit" className="btn-primary">
-                Register
-            </button>
-        </form>
-          {/*link  to Rgister page*/}
-        <p className="link-text">
-            Don't have an account? <Link to="/login">login here</Link>
-        </p>
+  const [formData, setFormData] = React.useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const navigate =useNavigate()
+
+  //logic section
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Full name is required.";
+    } else if (formData.name.length <= 3) {
+      newErrors.name = "Minimum 3 characters required.";
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Email is invalid";
+    }
+
+    if (!formData.phone.trim()) {
+      newErrors.phone = "Phone number is required.";
+    } else if (!/^[0-9]{10}$/.test(formData.phone)) {
+      newErrors.phone = "Phone number is invalid";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required.";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Minimum 6 characters required";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+  const handleInputChange = (e) => {
+    // console.log(e.target.name,e.target.value);
+
+    //e.target.name gives the name attribute of the input field
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    setErrors({
+    ...errors,
+    [e.target.name]: "",
+  });
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      localStorage.setItem('authData',JSON.stringify(formData))
+      alert('Registration succesfull ....!')
+      navigate("/login")
+    }
+    console.log("Form submitted:", formData);
+  };
+
+  // useEffect(() => {
+  // console.log(formData)
+  // },[formData]);
+
+  //design section
+  return (
+    <div className="form-container">
+      {/* Page Title */}
+      <h1 className="form-title">REGISTER</h1>
+      <form onSubmit={handleSubmit}>
+        {/* Nmae Field */}
+        <div className="form-group">
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            placeholder="Enter your full name"
+            onChange={handleInputChange}
+          />
+          {errors.name && <span className="error">{errors.name}</span>}
         </div>
-    </>
-    );
+        {/* Email Field */}
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            placeholder="Enter your email address"
+            onChange={handleInputChange}
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+
+        {/*Phone Number Field */}
+        <div className="form-group">
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            placeholder="Enter your phone number"
+            onChange={handleInputChange}
+          />
+          {errors.phone && <span className="error">{errors.phone}</span>}
+        </div>
+
+        {/* Password Field */}
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            placeholder="Create a password"
+            onChange={handleInputChange}
+          />
+          {errors.password && <span className="error">{errors.password}</span>}
+        </div>
+
+        {/* Submit Button */}
+        <button onClick={validate} type="submit" className="btn-primary">
+          Register
+        </button>
+      </form>
+      {/* Link to Login Page */}
+      <p className="link-text">
+        Already have an account? <a href="/Login">Login here</a>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
