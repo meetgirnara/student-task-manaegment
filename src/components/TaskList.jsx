209399b1b58bcdc6601d6684c59081a2b4796274
manav girnara <manavgirnara@gmail.com>
2026-02-05 10:14:21 +0530

    changes are added

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,45 +1,52 @@
 import React from "react";
-import "../index.css";
 
-const TaskList = () => {
-    return (
+const TaskList = ({ tasks }) => {
+  return (
+    <>
     <div className="task-grid">
-      {/* Task Card 1 */}
-        <div className="task-card" style={{ Position: "relative" }}>
-        <h3>Complete React Assignment</h3>
-        <p>Finish task maneger UI and styling</p>
+      {/*Task card 1 */}
+        {tasks.map((task) => (
+            <div className="task-card" style={{ position: "relative" }}>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
 
-        <div className="task-meta">
-            <span>Date: 2026-02-10</span>
-            <span className="priority-high priority-high">High</span>
-        </div>
+            <div className="task-meta">
+              <span>Due: {task.dueDate}</span>
+              <span className={`priority-badge priority-${task.priority.toLowerCase()}`}>{task.priority}</span>
+            </div>
 
-        <div className="task-action">
-            <button
-            className="btn-icon"
-            style={{ background: "#00d2ff" }}
-            title="Edit Task"
-            >
-            ✏️
-            </button>
-            <button
-            className="btn-icon"
-            style={{ background: "#00b894" }}
-            title="Mark Complete"
-            >
-            ✔️
-            </button>
-            <button
-            className="btn-icon"
-            style={{ background: "red" }}
-            title="Delete Task"
-            >
-            🗑️
-            </button>
-        </div>
-        </div>
+            <div className="task-action">
+              <button
+                className="btn-icon"
+                style={{ background: "#00d2ff" }}
+                title="Edit Task"
+              >
+                ✏️
+              </button>
+
+              <button
+                className="btn-icon"
+                style={{ background: "#00b894" }}
+                title="Mark Complete"
+              >
+                ✔️
+              </button>
+
+              <button
+                className="btn-icon"
+                style={{ background: "red" }}
+                title="Delete Task"
+              >
+                🗑️
+              </button>
+            </div>
+            </div>
+        
+        ))}
+    
     </div>
-    );
+    </>
+  );
 };
 
 export default TaskList;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,39 +3,41 @@ import Navbar from "../components/Navbar";
 import TaskList from "../components/TaskList";
 import { useNavigate } from "react-router-dom";
 
-const Dashboard =() => {
-const navigate = useNavigate();
-const[tasks, setTasks]=useState([])
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
 
-useEffect(()=>{
-    fetchdata()
-},[])
-
-    
-    const fetchdata = async() =>{
-        try{
-        const respone = await fetch ("http://localhost:3000/tasks");
-        const data = respone.json()
-        setTasks(data);
-    }catch(error){
-        console.log(error)
+  const fetchdata = async () => {
+    try {
+      const respone = await fetch("http://localhost:3000/tasks");
+      const data = await respone.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
     }
-    };
+  };
+
+  useEffect(() => {
+    console.log('called after API',tasks)
+  }, [tasks])
+  useEffect(() => {
+    fetchdata();
+  }, []);
 
 
-    const handleLogout = () =>{
-        localStorage.removeItem("loginData")
-        // localStorage.removeItem("authData")
-        // localStorage.clear()
-        navigate('/login')
-    }
-    return (
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    // localStorage.removeItem("authData")
+    // localStorage.clear()
+    navigate("/login");
+  };
+  return (
     <div>
-        <Navbar title="Task Manegment" onLogout={handleLogout}/>
-        <h1>MY TASKS</h1>
-        <TaskList/>
+      <Navbar title="Task Manegment" onLogout={handleLogout} />
+      <h1>MY TASKS</h1>
+      <TaskList tasks={tasks} />
     </div>
-    );
-}
+  );
+};
 
 export default Dashboard;
